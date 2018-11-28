998332d2e4d70f78a6bd83b4f5a2c3e430049ad3
Joseph <anthasu1@gmail.com>
2018-11-27 20:59:35 -0500

    Push content, styling added, jQuery added

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function() {
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 50) {
+          $('body').addClass('bg-change')
+          $('.init-title').addClass('hidden')
+          $('.scroll-title').removeClass('hidden')
+         // $('.menu-item').addClass('scroll-color-change')
+          $('.sticky-nav').addClass('scroll-menu-bg')
+        }
+       if ($(this).scrollTop() < 50) {
+          $('body').removeClass('bg-change')
+          $('.init-title').removeClass('hidden')
+          $('.scroll-title').addClass('hidden')
+        //  $('.menu-item').removeClass('scroll-color-change')
+          $('.sticky-nav').removeClass('scroll-menu-bg')
+        }
+    });
+ });
+ 
